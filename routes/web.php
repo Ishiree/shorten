7819a243082a2d92f0b8e2d8571bda43da90dd5d
Hadyan Ahmad <hadyandev@gmail.com>
2021-04-21 20:00:22 +0700

    Redirect root URL
    Redirect root URL to griyayatim.com

--- routes/web.php
+++ routes/web.php
@@ -7,8 +7,8 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
-    // return Redirect::to('https://griyayatim.com/');
-    return view('welcome');
+    return Redirect::to('https://griyayatim.com/');
+    // return view('welcome');
 });
 
 Auth::routes();
